148ecf8fd3ee49037d216c2338fb4e791a475973
Startos <baryshev.k.s@mail.ru>
2021-03-22 22:35:07 +0700

    Added first simple tests

--- src/test/java/LruTest.java
+++ src/test/java/LruTest.java
@@ -6,15 +6,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import ru.baryshev.kirill.AlgorithmLFU;
+import ru.baryshev.kirill.AlgorithmLRU;
 import ru.baryshev.kirill.MySuperCache;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext-test.xml"})
-public class LfuTest {
+public class LruTest {
 
     @Autowired
-    @Qualifier("cacheLFU")
+    @Qualifier("cacheLRU")
     MySuperCache cache;
 
     @Value("${cache.cacheSize}")
@@ -22,16 +22,6 @@ public class LfuTest {
 
     @Test
     public void testNameOfAlgorithm() {
-        Assert.assertEquals(cache.getAlgorithmName(), AlgorithmLFU.class.getSimpleName());
-    }
-
-    @Test
-    public void testMaxSize() {
-
-        cache.put(1L, "Long 1");
-        cache.put(2L, "Long 2");
-        cache.put(3L, "Long 3");
-        cache.put(4L, "Long 4");
-        Assert.assertEquals(cache.currentSize(), expectedSize);
+        Assert.assertEquals(cache.getAlgorithmName(), AlgorithmLRU.class.getSimpleName());
     }
 }
