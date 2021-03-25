ebf9c153f9c9d166b23e7124c0155a86bebb585d
Kirill.Baryshev <baryshev.k.s@mail.ru>
2021-03-25 16:59:49 +0700

    Added 2 realization of Cache.java^ CacheLFU.java CacheLRU.java

---
+++ src/main/java/ru/baryshev/kirill/CacheLFU.java
@@ -0,0 +1,78 @@
+package ru.baryshev.kirill;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Log4j
+public class CacheLFU<K, V> implements Cache<K, V> {
+
+    /**
+     * Мапа для хранения данных кеша
+     */
+    private Map<K, V> mapWithValue = new HashMap<>();
+    /**
+     * Мапа для хранения частоты использования различных элементов.
+     * Необходим для реализации LFU
+     */
+    private Map<K, Integer> mapWithCount = new HashMap<>();
+    /**
+     * Максимальный размер кеша
+     */
+    private Integer maxSize;
+
+    public CacheLFU(Integer maxSize) {
+        if (maxSize <= 0) {
+            log.error("Максимальный размер хеша должен быть больше 0");
+            throw new IllegalArgumentException("Максимальный размер хеша должен быть больше 0");
+        }
+        this.maxSize = maxSize;
+    }
+
+
+    /**
+     * Алгоритм удаления для LFU
+     */
+    private void removeValue() {
+        K keyOfMinValue = null;
+        Integer minValue = Integer.MAX_VALUE;
+        for (Map.Entry<K, Integer> entry : mapWithCount.entrySet()) {
+            if (minValue > entry.getValue()) {
+                minValue = entry.getValue();
+                keyOfMinValue = entry.getKey();
+            }
+        }
+        V valueOfMinValue = mapWithValue.get(keyOfMinValue);
+        mapWithValue.remove(keyOfMinValue);
+        mapWithCount.remove(keyOfMinValue);
+        log.info(String.format("Из кеша удален элемент. Ключ: %s; Значение: %s; Количество использований: %s.",
+                keyOfMinValue, valueOfMinValue, minValue));
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (mapWithValue.size() == maxSize) {
+            removeValue();
+        }
+        mapWithValue.put(key, value);
+        mapWithCount.put(key, 1);
+        log.info(String.format("В LFU кеш добавлен элемент. Ключ: %s; Значение: %s.", key, value));
+    }
+
+    @Override
+    public V get(K key) {
+        if (!mapWithValue.containsKey(key)) {
+            log.error("В кеше не найден элемент с ключом: " + key);
+            return null;
+        }
+        mapWithCount.replace(key, mapWithCount.get(key) + 1);
+        log.info(String.format("В кеше увеличилось количество обращений к ключу: %s на 1. Текущее значение: %s", key, mapWithCount.get(key)));
+        return mapWithValue.get(key);
+    }
+
+    @Override
+    public Integer currentSize() {
+        return mapWithValue.size();
+    }
+}
---
+++ src/main/java/ru/baryshev/kirill/CacheLRU.java
@@ -0,0 +1,79 @@
+package ru.baryshev.kirill;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@Log4j
+public class CacheLRU<K, V> implements Cache<K, V> {
+    /**
+     * Мапа для хранения данных кеша
+     */
+    private Map<K, V> mapWithValue = new HashMap<>();
+    /**
+     * Максимальный размер кеша
+     */
+    private Integer maxSize;
+    /**
+     * Из-за особенности линкед листа в нем будут храниться ключи.
+     */
+    private List<K> list = new LinkedList<>();
+
+    public CacheLRU(Integer maxSize) {
+        if (maxSize <= 0) {
+            log.error("Максимальный размер хеша должен быть больше 0");
+            throw new IllegalArgumentException("Максимальный размер хеша должен быть больше 0");
+        }
+        this.maxSize = maxSize;
+    }
+
+    /**
+     * Добавление элемента
+     *
+     * @param key   Ключ
+     * @param value Значение
+     */
+    public void put(K key, V value) {
+        if (mapWithValue.size() == maxSize) {
+            removeValue();
+        }
+
+        mapWithValue.put(key, value);
+        list.add(key);
+        log.info(String.format("В кеш добавлен элемент. Ключ: %s; Значение: %s.", key, value));
+    }
+
+    /**
+     * Полчение элемента по ключу
+     *
+     * @param key Ключ
+     */
+    public V get(K key) {
+        if (!mapWithValue.containsKey(key)) {
+            log.error("В кеше не найден элемент с ключом: " + key);
+            return null;
+        }
+        list.remove(key);
+        list.add(key);
+        log.info(String.format("В кеше \"Возраст\" для ключа %s был сброшен!", key));
+        return mapWithValue.get(key);
+    }
+
+    @Override
+    public Integer currentSize() {
+        return mapWithValue.size();
+    }
+
+    /**
+     * Алгоритм удаления для LRU
+     */
+    public void removeValue() {
+        int firstElemIndex = 0;
+        K keyForRemove = list.get(firstElemIndex);
+        mapWithValue.remove(keyForRemove);
+        list.remove(firstElemIndex);
+    }
+}
